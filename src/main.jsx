5ad41b6a75b01eee2ac33c364f939da9ca9f605b
jsprogram2014 <jsprogram2014@gmail.com>
2025-06-21 11:03:19 -0400

    created first hello world all by pure coding

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDom from 'react-dom/client';
+import React from 'react';
+import SimpleClass from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root=ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<SimpleClass/>)
+
+
+
+
